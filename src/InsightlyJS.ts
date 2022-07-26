8397b2d17acfdf9a836ad125024ab3fc029eb2a7
j-labbe <86276115+j-labbe@users.noreply.github.com>
2022-07-26 17:25:16 -0400

    Consolidations, fixes

--- src/InsightlyJS.ts
+++ src/InsightlyJS.ts
@@ -1,5 +1,4 @@
-import { ActivitySet } from './types/ActivitySet';
-import { FileAttachment, Comment } from './types/Comments';
+import { ActivitySet, FileAttachment, Comment } from './types';
 import runtimeIsCompatible from './utils/runtimeIsCompatible';
 import * as ActivitySets from './ActivitySets';
 import * as Comments from './Comments';
@@ -10,13 +9,33 @@ class InsightlyJS {
     private readonly apiKey: string;
     private readonly apiUrl: string;
 
-    constructor(options: { apiKey: string; apiHost: string }) {
+    /**
+     * Creates a new InsightlyJS instance.
+     * @param options.apiKey The API key for the Insightly account
+     * @param options.apiUrl The API URL for the Insightly account
+     * @throws Error if the runtime is not compatible
+     * @info Fetch API polyfills will be used if needed
+     */
+    constructor(options: { apiKey: string; apiUrl: string }) {
         this.apiKey = options.apiKey;
-        this.apiUrl = options.apiHost;
+        this.apiUrl = options.apiUrl;
 
-        if (!runtimeIsCompatible()) {
-            throw new Error('InsightlyJS is not compatible with this runtime. Please ugrade your browser or Node version (18+).');
+        runtimeIsCompatible().then((isCompatible) => {
+            if (!isCompatible) {
+                throw new Error('InsightlyJS is not compatible with this runtime. Please upgrade your browser or Node version (18+).');
+            }
+        });
+
+        if (!options.apiKey || !options.apiUrl) {
+            throw new Error('InsightlyJS: apiKey and apiUrl are required.');
         }
+
+        this.getActivitySetList = this.getActivitySetList.bind(this);
+        this.getActivitySet = this.getActivitySet.bind(this);
+        this.getCommentFileAttachments = this.getCommentFileAttachments.bind(this);
+        this.addCommentFileAttachment = this.addCommentFileAttachment.bind(this);
+        this.updateComment = this.updateComment.bind(this);
+        this.deleteComment = this.deleteComment.bind(this);
     }
 
     /**************************************************************************
@@ -26,22 +45,13 @@ class InsightlyJS {
      *************************************************************************/
 
     /**
-     * @abstract Activity Set API
-     * @public
-     */
-    public activitySets = {
-        getActivitySetList: this.getActivitySetList,
-        getActivitySet: this.getActivitySet,
-    };
-
-    /**
      * @param brief Only return the top level properties of the ActivitySet
      * @param skip Number of ActivitySets to skip
      * @param top Maximum number of ActivitySets to return
      * @param countTotal Return the total number of ActivitySets
      * @see https://api.insightly.com/v3.1/Help#!/ActivitySets/GetActivitySets
      */
-    private async getActivitySetList(brief?: boolean, skip?: number, top?: number, countTotal?: boolean): Promise<ActivitySet[]> {
+    public async getActivitySetList(brief?: boolean, skip?: number, top?: number, countTotal?: boolean): Promise<ActivitySet[]> {
         return await ActivitySets.getActivitySetList(this.apiKey, this.apiUrl, brief, skip, top, countTotal);
     }
 
@@ -49,7 +59,7 @@ class InsightlyJS {
      * @param id The ID of the ActivitySet
      * @see https://api.insightly.com/v3.1/Help#!/ActivitySets/GetActivitySet
      */
-    private async getActivitySet(id: number): Promise<ActivitySet> {
+    public async getActivitySet(id: number): Promise<ActivitySet> {
         return await ActivitySets.getActivitySet(this.apiKey, this.apiUrl, id);
     }
 
@@ -59,13 +69,6 @@ class InsightlyJS {
      **************************************************************************
      *************************************************************************/
 
-    public comments = {
-        getFileAttachments: this.getFileAttachments,
-        addFileAttachment: this.addFileAttachment,
-        updateComment: this.updateComment,
-        deleteComment: this.deleteComment,
-    };
-
     /**
      * @param id The ID of the Comment
      * @param updatedAfterUtc Earliest date when the file attachment was last updated
@@ -74,7 +77,7 @@ class InsightlyJS {
      * @param countTotal Return the total number of records
      * @see https://api.insightly.com/v3.1/Help#!/Comments/GetFileAttachments
      */
-    private async getFileAttachments(
+    public async getCommentFileAttachments(
         id: number,
         updatedAfterUtc?: string,
         skip?: number,
@@ -90,7 +93,7 @@ class InsightlyJS {
      * @param fileAttachment The file attachment to add
      * @see https://api.insightly.com/v3.1/Help#!/Comments/AddFileAttachment
      */
-    private async addFileAttachment(id: number, fileAttachment: FileAttachment): Promise<FileAttachment> {
+    public async addCommentFileAttachment(id: number, fileAttachment: FileAttachment): Promise<FileAttachment> {
         return await Comments.addFileAttachment(this.apiKey, this.apiUrl, id, fileAttachment);
     }
 
@@ -99,7 +102,7 @@ class InsightlyJS {
      * @param id The ID of the Comment
      * @param comment The comment to update
      */
-    private async updateComment(id: number, comment: Comment): Promise<Comment> {
+    public async updateComment(id: number, comment: Comment): Promise<Comment> {
         return await Comments.updateComment(this.apiKey, this.apiUrl, id, comment);
     }
 
@@ -108,7 +111,7 @@ class InsightlyJS {
      * @param id The ID of the Comment
      * @returns {boolean} True if the comment was successfully deleted, false otherwise
      */
-    private async deleteComment(id: number): Promise<boolean> {
+    public async deleteComment(id: number): Promise<boolean> {
         return await Comments.deleteComment(this.apiKey, this.apiUrl, id);
     }
 }
